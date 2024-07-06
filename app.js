a3dc95d769e1f4374cf7973d88eaa1467e6a504b
Sandhyarani Mohanty <155060146+sandhya-mohanty@users.noreply.github.com>
2024-07-06 18:20:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: "https://accredian-frontend-task-oth9.vercel.app ",
+  origin: ["https://accredian-frontend-task-oth9.vercel.app "],
         methods: ["POST", "GET"],
         credentials: true
 }));
